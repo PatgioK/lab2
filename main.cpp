ac23498ec6552f42ce1e68bb964e615ddaffed76
Patgio Kwok <Patgiok@gmail.com>
2018-09-21 15:58:57 -0700

    read file error: reading ints

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void readFile();
 int main() {
 
     std::ofstream reading;
+    reading.open("Readings.txt");
 
 
     double min = 50;
@@ -29,5 +30,45 @@ int main() {
         std::cout << i << std::setw(10) << std::fixed << std::setprecision(3) << randnum << std::endl;
         reading << i << std::setw(10) << std::fixed << std::setprecision(3) << randnum << std::endl;
     }
+    readFile();
     return 0;
+}
+
+void readFile() {
+
+    std::string str;
+    std::ifstream book;
+
+    book.open("Readings.txt");
+    int count;
+    double average;
+    double median;
+    double largest = 50;
+    double lowest = 90;
+    double number;
+    double total = 0;
+    int intnumber;
+    std::vector<double> read;
+
+    while(book >> number) {
+        count++;
+        book >> intnumber >> number;
+        if (number > largest) {
+            largest = number;
+        } else if (number < lowest) {
+            number = lowest;
+        }
+        total += number;
+        read.push_back(number);
+
+        int size = read.size();
+        std::sort(read.begin(), read.end());
+        median = read[floor(size / 2.0)];
+    }
+    average = total / count;
+    std::cout << "there are " << count << " readings in the file." << std::endl;
+    std::cout << "the average reading is " << std::fixed << std::setprecision(3) << average << std::endl;
+    std::cout << "the highest reading is " << std::fixed << std::setprecision(3) << largest << std::endl;
+    std::cout << "the lowest reading is " << std::fixed << std::setprecision(3) << lowest << std::endl;
+    std::cout << "the median reading is " << std::fixed << std::setprecision(3) << median << std::endl;
 }
