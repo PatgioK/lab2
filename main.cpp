7fb3f55b53ff782339198c5d1c496e12f3bef3a0
Patgio Kwok <Patgiok@gmail.com>
2018-09-21 15:39:31 -0700

    generate nums

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
+
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <algorithm>
+
+void readFile();
 
 int main() {
-    std::cout << "Hello, World! modified output" << std::endl;
-    
+
+    std::ofstream reading;
+
+
+    double min = 50;
+    double max = 90;
+
+
+    std::default_random_engine generator(time(0));
+    std::uniform_real_distribution<double>
+            distribution(min, max);
+
+    srand(time(NULL));
+    int nums = (rand() % 513 + 512);
+    for (int i = 0; i < nums; i++) {
+        double randnum = distribution(generator);
+        std::cout << i << std::setw(10) << std::fixed << std::setprecision(3) << randnum << std::endl;
+        reading << i << std::setw(10) << std::fixed << std::setprecision(3) << randnum << std::endl;
+    }
     return 0;
 }
